cf2b54853490e2628baa7feaee4a5c35ae4b4db1
joseluisvaz <joseluisv1201@gmail.com>
2018-03-18 20:55:03 -0600

    Rename from LineDetectorImpl

--- catkin_ws/src/00_line_detection/line_detector/src/line_detector_node.py
+++ catkin_ws/src/00_line_detection/line_detector/src/line_detector_node.py
@@ -2,7 +2,7 @@
 
 import sys
 
-from line_detector import LineDetectorImpl
+from line_detector import line_detector_impl
 
 if __name__ == "__main__":
-    LineDetectorImpl.main(sys.argv)
+    line_detector_impl.main(sys.argv)
