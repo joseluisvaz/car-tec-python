10ee250eb13dbccd9798e980244341faf5f91d6d
joseluisvaz <joseluisv1201@gmail.com>
2018-03-18 20:49:50 -0600

    Wraps ros -> cv image conversions

--- catkin_ws/src/00_line_detection/line_detector/include/line_detector/cvbridge_example.py
+++ catkin_ws/src/00_line_detection/line_detector/include/line_detector/cvbridge_example.py
@@ -12,17 +12,13 @@ from cv_bridge import CvBridge, CvBridgeError
 class ImageConverter:
 
     def __init__(self):
-        self.frame_counter = 0
         self.bridge = CvBridge()
         self.image_sub = rospy.Subscriber("/zed/rgb/image_raw_color", Image, self.callback)
         self.image_pub = rospy.Publisher("/line_detector/output_image", Image, queue_size=1)
 
     def callback(self, data):
-        try:
-            cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
-            self.frame_counter += 1
-        except CvBridgeError as e:
-            print(e)
+
+        cv_image = self._to_cv_image(data)
 
         (rows, cols, channels) = cv_image.shape
         if cols > 60 and rows > 60:
@@ -31,8 +27,18 @@ class ImageConverter:
         cv2.imshow("Image window", cv_image)
         cv2.waitKey(3)
 
+        ros_img = self._to_ros_image(cv_image)
+        self.image_pub.publish(ros_img)
+
+    def _to_cv_image(self, data):
         try:
-            self.image_pub.publish(self.bridge.cv2_to_imgmsg(cv_image, "bgr8"))
+            return self.bridge.imgmsg_to_cv2(data, "bgr8")
+        except CvBridgeError as e:
+            print(e)
+
+    def _to_ros_image(self, data):
+        try:
+            return self.bridge.cv2_to_imgmsg(data, "bgr8")
         except CvBridgeError as e:
             print(e)
 
