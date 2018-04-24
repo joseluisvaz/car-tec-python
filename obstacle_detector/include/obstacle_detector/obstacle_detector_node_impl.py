e474ad3cd47471bf60cf7806b9377d989a71ee82
joseluisvaz <joseluisv1201@gmail.com>
2018-04-24 13:34:34 -0500

    Add first template for obstacle detector node

--- obstacle_detector/include/obstacle_detector/obstacle_detector_node_impl.py
+++ obstacle_detector/include/obstacle_detector/obstacle_detector_node_impl.py
@@ -9,13 +9,19 @@ from sensor_msgs.msg import Image
 from car_tec_msgs.msg import Segment
 from car_tec_msgs.msg import SegmentList
 from cv_bridge import CvBridge
+import depthimage_to_laserscan as laserimg
 
 
-class ObstacleDetector():
+class ObstacleDetectorImpl(object):
     def __init__(self):
         self.message = "hola"
 
-    
+
 def main():
-    obj = ObstacleDetector()
+    rospy.init_node("obstacle_detectot", anonymous=True)
+    obj = ObstacleDetectorImpl()
     print(obj.message)
+    try:
+        rospy.spin()
+    except KeyboardInterrupt:
+        print("Shutting down")
