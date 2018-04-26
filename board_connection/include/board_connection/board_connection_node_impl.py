e64dc2ea02368cfb855a69fdd24d7bb6959ab054
Jose Vazquez <joseluisv1201@gmail.com>
2018-04-26 17:28:28 -0500

    Feat/board connection (#17)
    * Modidy default file for board_connection package
    
    *  Add launch file for board_connection package
    
    * First implementation of a publisher in Board connection implementation
    
    * Add functionality for array of Floats
    
    * Add board launch node to launch file
    
    * Add launch file for board_connection wiithout serial com

--- board_connection/include/board_connection/board_connection_node_impl.py
+++ board_connection/include/board_connection/board_connection_node_impl.py
@@ -3,24 +3,36 @@
 from __future__ import print_function
 
 import rospy
-import cv2
 
-from sensor_msgs.msg import Image
 from car_tec_msgs.msg import Segment
 from car_tec_msgs.msg import SegmentList
-from cv_bridge import CvBridge
-import depthimage_to_laserscan as laserimg
+from std_msgs.msg import Float32MultiArray
 
 
 class BoardConnectionImpl(object):
     def __init__(self):
-        self.message = "hola"
+        self.segment_list = None
+        self.segment_sub = rospy.Subscriber(rospy.get_param("~subscriber_topic"),
+                                            SegmentList,
+                                            self.callback,
+                                            queue_size=rospy.get_param("~subs_queue_size"),
+                                            buff_size=rospy.get_param("~buff_size"))
+
+        self.control_pub = rospy.Publisher(rospy.get_param("~publisher_topic"),
+                                           Float32MultiArray,
+                                           queue_size=rospy.get_param("~pubs_queue_size"))
+
+    def callback(self, segment_list):
+        self.segment_list = segment_list
+        a = len(self.segment_list.segments)*1.111
+        array = Float32MultiArray()
+        array.data = [a, a]
+        self.control_pub.publish(array)
 
 
 def main():
     rospy.init_node("board_connection", anonymous=True)
     obj = BoardConnectionImpl()
-    print(obj.message)
     try:
         rospy.spin()
     except KeyboardInterrupt:
