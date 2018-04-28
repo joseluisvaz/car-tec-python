4b958d8b8cbb305206531224783f25d668aea51b
joseluisvaz <joseluisv1201@gmail.com>
2018-04-28 15:54:51 -0500

    Implement Warper object

--- birds_eye/include/birds_eye/birds_eye_node_impl.py
+++ birds_eye/include/birds_eye/birds_eye_node_impl.py
@@ -8,27 +8,49 @@ import cv2
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
 
-from birds_eye.utils import unwarp
+from car_tec_msgs.msg import SegmentList
+from birds_eye.utils.warper import Warper
 
 
 class BirdsEyeImpl(object):
     def __init__(self):
         self.bridge = CvBridge()
+        self.warper = Warper()
         self.segment_list = None
-        self.image_sub = rospy.Subscriber(rospy.get_param("~subscriber_topic"),
+        self.configured = False
+        self.image_sub = rospy.Subscriber(rospy.get_param("~subscriber_topic_1"),
                                           Image,
                                           self.callback,
                                           queue_size=rospy.get_param("~subs_queue_size"),
                                           buff_size=rospy.get_param("~buff_size"))
 
-        self.image_pub = rospy.Publisher(rospy.get_param("~publisher_topic"),
+        #self.segment_sub = rospy.Subscriber(rospy.get_param("~subscriber_topic_2"),
+        #                                    SegmentList,
+        #                                    self.callback,
+        #                                    queue_size=rospy.get_param("~subs_queue_size"),
+        #                                    buff_size=rospy.get_param("~buff_size"))
+
+        self.image_pub = rospy.Publisher(rospy.get_param("~publisher_topic_1"),
                                          Image,
                                          queue_size=rospy.get_param("~pubs_queue_size"))
 
+        self.segment_pub = rospy.Publisher(rospy.get_param("~publisher_topic_2"),
+                                           SegmentList,
+                                           queue_size=rospy.get_param("~pubs_queue_size"))
+
     def callback(self, img_msg):
-        cv_image = self.bridge.imgmsg_to_cv2(img_msg, "bgr8")
-        a, b, c = unwarp.corners_unwarp(cv_image)
-        ros_image = self.bridge.cv2_to_imgmsg(a, "bgr8")
+
+        img = self.bridge.imgmsg_to_cv2(img_msg, "bgr8")
+
+        if not self.configured:
+            self.warper.set_image_size(img)
+            self.warper.set_homography(img)
+            self.configured = True
+
+        warped_img = self.warper.warp_image(img)
+
+        ros_image = self.bridge.cv2_to_imgmsg(warped_img, "bgr8")
+
         self.image_pub.publish(ros_image)
 
 
