4ef10e6302f5d9c8e79fa825934bde9809225616
joseluisvaz <joseluisv1201@gmail.com>
2018-05-07 23:30:43 -0500

    Clear redundancy from linear regression fnuctions

--- lane_control/include/lane_control/utils/linear_regression.py
+++ lane_control/include/lane_control/utils/linear_regression.py
@@ -1,10 +1,6 @@
-import numpy as np
 from scipy.stats import linregress
 
 
-def fit_line(array, img_shape):
-
-    # Stack one line
-    array = np.concatenate((np.ones((array.shape[0], 1)), array), axis=1)
-    slope, intercept, r_value, p_value, std_err = linregress(array[:, 2], array[:, 1])
+def fit_line(array):
+    slope, intercept, r_value, p_value, std_err = linregress(array[:, 1], array[:, 2])
     return slope, intercept
