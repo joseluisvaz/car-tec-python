03634a7b7588a9e6ebf93e33dfac8b58fbaff537
joseluisvaz <joseluisv1201@gmail.com>
2018-05-17 19:22:09 -0500

    Modularization fo code, single and double line

--- lane_control/include/lane_control/tracker_interface.py
+++ lane_control/include/lane_control/tracker_interface.py
@@ -1,8 +1,9 @@
 from abc import ABCMeta, abstractmethod
 
+
 class TrackerInterface(object):
     __metaclass__ = ABCMeta
 
     @abstractmethod
-    def get_segments(self, segment_list):
+    def track_segments(self, segment_list):
         pass
